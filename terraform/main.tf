e066f73a6b231a2ab20921ca85b8b898bb68dcfe
高橋悟生 <g.takahashi@buysell-technologies.com>
2023-10-15 13:09:16 +0900

    init

---
+++ terraform/main.tf
@@ -0,0 +1,22 @@
+# TODO GCSのバケットを作成
+terraform {
+  backend "gcs" {
+    bucket  = "task-app-terraform-state"
+    prefix  = "terraform/state"
+    region  = "asia-northeast1" # バケットのリージョン
+  }
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google" # プロバイダーのソース
+      version = "5.1.0" # プロバイダーのバージョン
+    }
+  }
+}
+
+provider "google" {
+  project = "causal-analyst-402006" # プロジェクトIDを設定
+  region  = "asia-northeast1" # リージョンを設定
+  # credentials = file(local.credentials_path) # パスをサービスアカウントキーのJSONファイルに設定
+  # zone = "asia-northeast1-a" # ゾーンを設定
+}
